b3fb8c0fe5d8002bae30c802a2be30f89b2cf2e3
Eduard Predescu <eduard.florin.predescu@gmail.com>
2023-06-19 18:09:32 +0300

    reduce stylesheet size

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: "auto",
+    inlineStylesheets: "always",
   },
   experimental: {
     assets: true,
